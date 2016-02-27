e4457d605ef1765f9b6d1f2c9c2812bf503ebf94
Wei-Tsung Lin <fasthall@gmail.com>
2016-02-26 17:30:25 -0800

    Maven project

---
+++ src/main/java/cs274/rc/App.java
@@ -0,0 +1,13 @@
+/*
+ * @Author Wei-Tsung Lin
+ * @Date 02/26/2016
+ */
+package cs274.rc;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
